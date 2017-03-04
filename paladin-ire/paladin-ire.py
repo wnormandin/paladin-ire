48ca3b4e191b2339e2c7d43df7745938d284c84e
Bill Normandin <bill@normandindev.net>
2017-03-03 18:59:17 -0600

    added character creation to primary game script

--- paladin-ire/paladin-ire.py
+++ paladin-ire/paladin-ire.py
@@ -2,3 +2,19 @@
 # -*- coding: utf-8 -*-
 
 import logging
+import argparse
+
+# Module imports
+from scripts.player_creation import character_creation
+from player.player import Player
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c','--create',action='store_true',help='Create a new character')
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    args = parse_args()
+    if args.create:
+        p = Player()
+        character_creation(p)
